c219ee9da819fdb2f61bca779e313f40bf3e2401
Casey Thomas <cpthomas@gmail.com>
2013-07-17 17:15:56 -0400

    Add additional search logic

--- scripts/find-orgs.py
+++ scripts/find-orgs.py
@@ -5,6 +5,7 @@ sys.path.append("..")
 import csv
 import json
 import re
+from nltk import metrics
 
 candidates = {
     'Clarke': [],
@@ -13,7 +14,7 @@ candidates = {
     'Blackwell': [],
     'Jones': [],
     'Henon': [],
-    'Bobby': [], # Henon's org is called Bobby 11 or Bobby II
+    'Bobby': [],  # Henon's org is called Bobby 11 or Bobby II
     'Sanchez': [],
     'Sánchez': [],
     'Bass': [],
@@ -32,8 +33,19 @@ with open('../data/composite.txt', 'r') as f:
     reader = csv.reader(f, delimiter='\t', quotechar="\"")
     for row in reader:
         for candidate in candidates.keys():
-            if re.search(r'(?i)' + candidate, row[0], re.IGNORECASE) is not None and \
-             row[0].strip() not in candidates[candidate]:
-                candidates[candidate].append(row[0].strip())
+
+            # Look for Candidates' name, ignore case
+            if (re.search(r'(?i)' + candidate, row[0], re.IGNORECASE) is not None and
+                row[0].strip() not in candidates[candidate]):
+                    candidates[candidate].append(row[0].strip())
+
+            # Look for a word in the org's name that is simliar to Candidates' name.
+            # i.e. Clark is close to Clarke
+            parts = row[0].split(' ')
+            for part in parts:
+                if (metrics.edit_distance(part, candidate) <= 2 and
+                    row[0].strip() not in candidates[candidate]):
+                        candidates[candidate].append(row[0].strip())
+
 
 print json.dumps(candidates)
