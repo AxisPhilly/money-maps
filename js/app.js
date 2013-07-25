b8bbf8b15acf9e31480bef8ed00dfb3978279c8e
Casey Thomas <cpthomas@gmail.com>
2013-07-25 15:34:20 -0400

    Add initial backbone app

--- js/app.js
+++ js/app.js
@@ -2,6 +2,97 @@ if (typeof app === 'undefined' || !app) {
   var app = {};
 }
 
+app.Candidate = Backbone.Model.extend({
+  url: function() {
+    return 'data/' + this.get('slug') + '.json';
+  }
+});
+
+app.Candidates = Backbone.Collection.extend({
+  model: app.Candidate
+});
+
+app.CandidateView = Backbone.View.extend({
+  tagName: 'div',
+
+  initialize: function() {
+    this.template = _.template($('#candidate-view-template').html());
+  },
+
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+
+    return this;
+  }
+});
+
+app.CityView = Backbone.View.extend({
+
+});
+
+app.StateView = Backbone.View.extend({
+
+});
+
+app.NationalView = Backbone.View.extend({
+
+});
+
+app.SelectView = Backbone.View.extend({
+  tagName: 'ul',
+
+  initialize: function() {
+
+  },
+
+  render: function() {
+    this.collection.each(function(candidate) {
+      this.$el.append(new app.SelectItemView({ model: candidate }).render().el);
+    }, this);
+
+    return this;
+  }
+});
+
+app.SelectItemView = Backbone.View.extend({
+  tagName: 'li',
+
+  events: {
+    'click': 'add'
+  },
+
+  render: function() {
+    this.$el.html(this.model.get('slug'));
+
+    return this;
+  },
+
+  add: function() {
+    $('#candidates').append(new app.CandidateView({ model: this.model }).render().el);
+  }
+});
+
+app.Router = Backbone.Router.extend({
+  initialize: function() {
+    d3.json('data/candidates.json', function(data){
+      app.candidates = new app.Candidates(data);
+
+      app.selectView = new app.SelectView({ collection: app.candidates });
+      $('#select-view').append(app.selectView.render().el);
+
+      Backbone.history.start({ pushState:false, silent:true });
+    });
+
+    _.templateSettings = {
+      interpolate : /\{\{(.+?)\}\}/g
+    };
+  }
+});
+
+
+app.router = new app.Router();
+
+
 function generateMap(input, divId, year, mapId, mapType) { 
 
   if (mapType == "Ward") {
@@ -69,5 +160,5 @@ function generateMap(input, divId, year, mapId, mapType) {
       };
 }
 
-generateMap('data/data.json', '#state-map-1', '2008', 'pa_counties', "County");
-generateMap('data/data.json', '#city-map-1', '2007', 'philadelphia_wards', "Ward");
+//generateMap('data/data.json', '#state-map-1', '2008', 'pa_counties', "County");
+//generateMap('data/data.json', '#city-map-1', '2007', 'philadelphia_wards', "Ward");
