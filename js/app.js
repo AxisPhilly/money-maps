da215767a3005537e8fc112296501c0266b29d80
Casey Thomas <cpthomas@gmail.com>
2013-07-25 17:25:12 -0400

    Backbone + d3

--- js/app.js
+++ js/app.js
@@ -14,24 +14,80 @@ app.Candidates = Backbone.Collection.extend({
 
 app.CandidateView = Backbone.View.extend({
   tagName: 'div',
+  className: 'candidate',
 
   initialize: function() {
     this.template = _.template($('#candidate-view-template').html());
+    this.year = 2012;
+    this.cityView = new app.CityView({});
+    this.stateView = new app.StateView({});
   },
 
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
+    this.$el.find('.city').html(this.cityView.render().el);
+    this.$el.find('.state').html(this.stateView.render().el);
 
     return this;
   }
 });
 
 app.CityView = Backbone.View.extend({
+  tagName: 'div',
 
+  initialize: function() {
+    this.width = 370;
+    this.height = 406;
+    this.projection = d3.geo.mercator()
+          .center([-75.1182, 40.0032])
+          .scale(65000)
+          .translate([this.width / 2, this.height / 2]);
+    this.path = d3.geo.path()
+          .projection(this.projection);
+  },
+
+  render: function() {
+    var svg = d3.select(this.el).append('svg')
+          .attr('width', this.width)
+          .attr('height', this.height);
+
+    svg.append("g")
+      .selectAll("path")
+        .data(topojson.feature(app.wards, app.wards.objects['philadelphia_wards']).features)
+      .enter().append("path")
+        .attr("d", this.path);
+
+    return this;
+  }
 });
 
 app.StateView = Backbone.View.extend({
+  tagName: 'div',
 
+  initialize: function() {
+    this.width = 370;
+    this.height = 218;
+    this.projection = d3.geo.mercator()
+          .center([-77.590, 41.02])
+          .scale(3605)
+          .translate([this.width / 2, this.height / 2]);
+    this.path = d3.geo.path()
+          .projection(this.projection);
+  },
+
+  render: function() {
+    var svg = d3.select(this.el).append('svg')
+          .attr('width', this.width)
+          .attr('height', this.height);
+
+    svg.append("g")
+      .selectAll("path")
+        .data(topojson.feature(app.counties, app.counties.objects['pa_counties']).features)
+      .enter().append("path")
+        .attr("d", this.path);
+
+    return this;
+  }
 });
 
 app.NationalView = Backbone.View.extend({
@@ -80,7 +136,15 @@ app.Router = Backbone.Router.extend({
       app.selectView = new app.SelectView({ collection: app.candidates });
       $('#select-view').append(app.selectView.render().el);
 
-      Backbone.history.start({ pushState:false, silent:true });
+      d3.json('data/counties.json', function(data) {
+        app.counties = data;
+
+        d3.json('data/wards.json', function(data) {
+          app.wards = data;
+
+          Backbone.history.start({ pushState:false, silent:true });
+        });
+      });
     });
 
     _.templateSettings = {
@@ -89,76 +153,5 @@ app.Router = Backbone.Router.extend({
   }
 });
 
-
+// go!
 app.router = new app.Router();
-
-
-function generateMap(input, divId, year, mapId, mapType) { 
-
-  if (mapType == "Ward") {
-
-    var width = 370,
-      height = 406,
-      centered;
-
-    var projection = d3.geo.mercator()
-        .center([-75.1180, 40.0032])
-        .scale(65000)
-        .translate([width / 2, height / 2]);
-
-  }
-
-  else if (mapType == "County") {
-
-    var width = 370,
-    height = 218,
-    centered;
-
-    var projection = d3.geo.mercator()
-      .center([-77.590, 41.02])
-      .scale(3605)
-      .translate([width / 2, height / 2]);
-  }
-
-  var map = d3.map();
-
-  var path = d3.geo.path()
-      .projection(projection);
-
-  var svg = d3.select(divId).append("svg")
-      .attr("width", width)
-      .attr("height", height);  
-
-  svg.append("rect")
-      .attr("class", "background")
-      .attr("width", width)
-      .attr("height", height)
-
-  queue()
-      .defer(d3.json, "data/"+mapId+".json")
-      .defer(d3.json, input)
-      .await(ready);
-
-  var city = svg.append("g");
-  var allValues = new Array();
-    function ready(error, jsonGeo, data) {
-      _.each(data[year][mapType], function(value, index){
-        map.set(index.toUpperCase(), value);
-        allValues.push(value);
-      });              
-
-    maxValue = d3.max(allValues);
-
-    var quantize = d3.scale.quantize()
-      .domain([0, maxValue])
-      .range(d3.range(4).map(function(i) { return "d" + i + "-4"; }));   
-      city.selectAll("id")
-        .data(topojson.object(jsonGeo, jsonGeo.objects[mapId]).geometries)
-        .enter().append("path")
-          .attr("class", function(d) { return quantize(map.get(d.id))})
-          .attr("d", path);
-      };
-}
-
-//generateMap('data/data.json', '#state-map-1', '2008', 'pa_counties', "County");
-//generateMap('data/data.json', '#city-map-1', '2007', 'philadelphia_wards', "Ward");
